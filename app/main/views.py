2a04d55e9534e2cf13967be106b8e77f03839997
charity-bit <charitynyanchera@gmail.com>
2022-05-15 23:15:23 +0300

    created edit post page

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from unicodedata import category
 from flask_login import current_user
 
@@ -8,7 +9,18 @@ from .forms import PostForm
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+
+    
+    recents = []  # query recent posts
+    all = Post.query.all()
+    rev = all[::-1]
+
+    for i in range(3):
+        recents.append(rev[i])
+    
+       
+    
+    return render_template('index.html',recents = recents,all = all)
 
 @main.route('/add-post',methods = ['GET','POST'])
 def add_post():
@@ -33,3 +45,43 @@ def add_post():
 
 
     return render_template('admin/addpost.html',form = form)
+
+@main.route('/posts/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html',post = post)
+
+@main.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post = Post.query.get_or_404(id)
+    if post:
+        pass
+
+    else:
+        flash("post cannot be found",category="error")
+    return redirect(url_for('main.post',id=post.id))
+
+@main.route('/posts/edit-post/<int:id>',methods = ['GET','POST'])
+def edit_post(id):
+    post = Post.query.get_or_404(id)
+    current_user_id = current_user.id
+    form = PostForm()
+
+    if request.method == 'POST':          
+        if  current_user_id !=1:
+            flash('You cannot edit this post',category="error")
+        else:
+            if post:
+                post.title = form.title.data
+                post.text = form.body.data 
+
+                post.save_post()
+
+                flash("Post has been updated successfully",category="success")
+
+                return redirect(url_for('main.post',id=post.id))
+    
+            else:
+                flash('Post cannot be found',category="error")
+
+    return render_template('admin/editpost.html',form = form)
